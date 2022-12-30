4b535121164f64857baac171053e70fa1a697fb6
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-12-30 16:46:56 +0100

    bg and text colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
     import '../app.css'
 </script>
 
+<header class="text-white m-5 text-center">
+    <h1 class="text-3xl">Checkers</h1>
+    <h2 class="text-xl m-5">White's Turn</h2>
+</header>
+
 <slot />
